aa9efeb0b199a9073d84e60b69f6a7bfd97bd41b
BatyrOvezdurdyyev <batyr.ovezdurdyyev@gmail.com>
2018-05-17 12:23:36 +0300

    Add downloadFromInternet and caching method

--- Countries/ViewControllers/CountriesVC.swift
+++ Countries/ViewControllers/CountriesVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import ObjectMapper
 
 class CountriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -14,6 +16,8 @@ class CountriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
     @IBOutlet weak var errorMessageLabel: UILabel!
     
     var countries: [Country] = []
+    var nextPageUrl: String  = ""
+    var key                  = "cachedKey"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +30,53 @@ class CountriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         tableView.dataSource          = self
         tableView.separatorStyle      = .singleLine
         tableView.backgroundColor     = UIColor.white
+    }
+    
+    func getCountriesFromCacheOrUrl() {
+        if userDefaults.object(forKey: key) != nil {
+            getAllCountriesFromCache()
+        } else {
+            let urlString = "https://rawgit.com/NikitaAsabin/799e4502c9fc3e0ea7af439b2dfd88fa/raw/7f5c6c66358501f72fada21e04d75f64474a7888/page1.json"
+            getAllCountriesFromServer(urlString: urlString)
+        }
+    }
+    
+    func getAllCountriesFromCache() {
+        
+        guard let data      = userDefaults.object(forKey: key) as? NSData else{ return }
+        guard let countries = NSKeyedUnarchiver.unarchiveObject(with: data as Data) as? [Country] else { return}
+        self.countries      = countries
+        
+        self.tableView.reloadData()
+    }
+    
+    func getAllCountriesFromServer(urlString: String) {
+        Alamofire.request(urlString).responseJSON { (response) in
+            if let data = response.result.value as? Dictionary<String, AnyObject> {
+                
+                self.errorMessageLabel.isHidden = true
+                self.tableView.isHidden         = false
+                
+                let countriesResponse = CountriesResponse(JSON: data)
+                guard let countries   = countriesResponse?.countries else {return}
+                guard let next        = countriesResponse?.next else {return}
+                
+                if countries.count > 0 {
+                    self.countries   = self.countries + countries
+                    let datalar      = NSKeyedArchiver.archivedData(withRootObject: self.countries)
+                    userDefaults.set(datalar, forKey: self.key) //ex: jsonPage_0
+                    self.nextPageUrl = next
+                    self.tableView.reloadData()
+                }
+            } else {
+                print("error in getting data in CountriesVC")
+                if (userDefaults.object(forKey: self.key) == nil) {
+                    self.tableView.isHidden         = true
+                    self.errorMessageLabel.isHidden = false
+                    self.errorMessageLabel.text     = "Can not connect to Internet"
+                }
+            }
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
